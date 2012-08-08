29a082abf92f2aaf7c34e0784d20aee86cd4f0e9
Daniel Brolund <daniel.brolund@agical.com>
2012-08-08 20:28:35 +0200

    Changed config and gitignore

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ exports.config = {
   redisPort: (process.env.ES_REDIS_PORT === undefined ? 6379 : process.env.ES_REDIS_PORT),
   redisPwd:  (process.env.ES_REDIS_PWD  === undefined ? '' : process.env.ES_REDIS_PWD),
   host:      (process.env.ES_HOST       === undefined ? 'localhost' : process.env.ES_HOST),
-  port:      (process.env.ES_PORT       === undefined ? 4000 : process.env.ES_PORT)
+  port:      (process.env.ES_PORT       === undefined ? 80 : process.env.ES_PORT)
 };
